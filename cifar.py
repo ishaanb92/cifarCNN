6671cb1d21fdf9cd1546933c62ffe408ac13238f
Ishaan Bhat <ibhat@grill.win.tue.nl>
2017-04-28 18:55:39 +0200

    Added loss, training computations. Poor results, need to debug

--- cifar.py
+++ cifar.py
@@ -2,6 +2,9 @@ import numpy as np
 import tensorflow as tf
 import data_helpers # Helper functions to fetch CIFAR data
 
+# Some defines
+MAX_STEPS = 10000
+BATCH_SIZE = 100
 
 #Helper functions
 def weights_initialize(shape):
@@ -16,7 +19,7 @@ cifar_dataset = data_helpers.load_data() # Loads training images/labels + test i
 
 # Inputs and labels
 image = tf.placeholder(tf.float32,[None,3072])
-label = tf.placeholder(tf.int32,[None])
+labels = tf.placeholder(tf.int64,[None])
 
 
 
@@ -63,13 +66,27 @@ fc_2 = tf.nn.relu(tf.matmul(fc_1, W_fc2) + b_fc2)
 # Output Layer
 W_out = weights_initialize([192,10])
 b_out = bias_initialize([10])
-out = tf.nn.relu(tf.matmul(fc_2, W_out) + b_out) # Output is a 1-D vector with 10 elements ( = #classes)
+# Not applied the non-linearity yet for the output. Softmax to model "inhibition", suppresses multiple activations
+out = tf.add(tf.matmul(fc_2, W_out),b_out) # Output is a 1-D vector with 10 elements ( = #classes)
+
+# Cost Model
+loss = tf.reduce_mean( tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=out))
+
+# Training step computation
+learning_rate = 0.1
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+# Evaluating the training
+correct_prediction = tf.equal(tf.argmax(out, 1), labels)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
 
 init = tf.global_variables_initializer()
 
 sess = tf.Session()
 
 sess.run(init);
+
 
 print ("Input size : ")
 print (image_reshape.get_shape())
@@ -103,3 +120,17 @@ print("\n")
 print ("Output layer size: ")
 print(out.get_shape())
 print("\n")
+
+# Training
+for i in range(MAX_STEPS):
+    # Generate batch
+    batch = np.random.choice(cifar_dataset['images_train'].shape[0], BATCH_SIZE)
+    images_batch = cifar_dataset['images_train'][batch]
+    labels_batch = cifar_dataset['labels_train'][batch]
+    # Execute the train step
+    sess.run(train_step,feed_dict = {image:images_batch,labels:labels_batch})
+    if i%100 == 0:
+        train_accuracy = sess.run(accuracy,feed_dict = {image:images_batch,labels:labels_batch})
+        print ('Step {:5d}: training accuracy {:g}'.format(i, train_accuracy))
+
+
