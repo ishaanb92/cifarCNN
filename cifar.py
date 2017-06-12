7a880de09b220c61ca0b0957c31cabcfd996f406
Ishaan Bhat <ibhat@grill.win.tue.nl>
2017-06-13 00:03:42 +0200

    Added batch normalization to the model, normalization added before non-linearity of every layer

--- cifar.py
+++ cifar.py
@@ -47,7 +47,16 @@ def inference(image):
     Wconv1 = weights_initialize([5,5,3,64],5e-2,0.0,"Wconv1")
     bconv1 = bias_initialize([64],"bconv1")
     conv1 = tf.nn.conv2d(image,Wconv1,[1,1,1,1],padding = 'SAME')
-    layer_1 = tf.nn.relu(tf.nn.bias_add(conv1,bconv1))
+    # gamma : Scale parameter
+    # beta : Shift parameter
+    # These trainable parameters are introduced so that the model while reducing covariate shift
+    # does not lose it's representational power (Names are consistent with notation in the BN paper [2015])
+    gamma_conv1 = tf.Variable(tf.ones([64]))
+    beta_conv1 = tf.Variable(tf.zeros([64]))
+    # Apply BN
+    batch_mean_conv1, batch_var_conv1 = tf.nn.moments(conv1,[0])
+    conv1_BN = tf.nn.batch_normalization(conv1,batch_mean_conv1,batch_var_conv1,beta_conv1,gamma_conv1,variance_epsilon=1e-3)
+    layer_1 = tf.nn.relu(tf.nn.bias_add(conv1_BN,bconv1))
 
     # Pooling
     pool1 = tf.nn.max_pool(layer_1,ksize = [1,3,3,1], strides = [1,2,2,1], padding = 'SAME')
@@ -59,7 +68,13 @@ def inference(image):
     Wconv2 = weights_initialize([5,5,64,64],0.1,0.0,"Wconv2")
     bconv2 = bias_initialize([64],"bconv2")
     conv2 = tf.nn.conv2d(norm1,Wconv2,[1,1,1,1],padding = 'SAME')
-    layer_2 = tf.nn.relu(tf.nn.bias_add(conv2,bconv2))
+    # Add BN here
+    gamma_conv2 = tf.Variable(tf.ones([64]))
+    beta_conv2 = tf.Variable(tf.zeros([64]))
+    # Apply BN
+    batch_mean_conv2, batch_var_conv2 = tf.nn.moments(conv2,[0])
+    conv2_BN = tf.nn.batch_normalization(conv2,batch_mean_conv2,batch_var_conv2,beta_conv2,gamma_conv2,variance_epsilon=1e-3)
+    layer_2 = tf.nn.relu(tf.nn.bias_add(conv2_BN,bconv2))
 
     #Normalize
     norm2  = tf.nn.lrn(layer_2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
@@ -71,12 +86,25 @@ def inference(image):
     W_fc1 = weights_initialize([pool2.get_shape()[1].value*pool2.get_shape()[2].value*64,384],0.04,0.004,"W_fc1") # 384 taken from original CIFAR classifier
     b_fc1 = bias_initialize([384],"b_fc1");
     pool2_flat = tf.reshape(pool2,[TRAINING_BATCH_SIZE,pool2.get_shape()[1].value*pool2.get_shape()[2].value*64])
-    fc_1 = tf.nn.relu(tf.matmul(pool2_flat, W_fc1) + b_fc1)
+    pool2_mul = tf.matmul(pool2_flat, W_fc1)
+    gamma_pool2 = tf.Variable(tf.ones([384]))
+    beta_pool2 = tf.Variable(tf.zeros([384]))
+    # Apply BN
+    batch_mean_pool2, batch_var_pool2 = tf.nn.moments(pool2_mul,[0])
+    pool2_BN = tf.nn.batch_normalization(pool2_mul,batch_mean_pool2,batch_var_pool2,beta_pool2,gamma_pool2,variance_epsilon=1e-3)
+    fc_1 = tf.nn.relu(tf.nn.bias_add(pool2_BN,b_fc1))
 
     # FC 2 Layer
     W_fc2 = weights_initialize([384,192],0.004,0.004,"W_fc2") # Shape taken from original CIFAR classifier
     b_fc2 = bias_initialize([192],"b_fc2");
-    fc_2 = tf.nn.relu(tf.matmul(fc_1, W_fc2) + b_fc2)
+    fc2_mul = tf.matmul(fc_1, W_fc2)
+    # Add BN here
+    gamma_fc2 = tf.Variable(tf.ones([192]))
+    beta_fc2 = tf.Variable(tf.zeros([192]))
+    # Apply BN
+    batch_mean_fc2, batch_var_fc2 = tf.nn.moments(fc2_mul,[0])
+    pool2_BN = tf.nn.batch_normalization(fc2_mul,batch_mean_fc2,batch_var_fc2,beta_fc2,gamma_fc2,variance_epsilon=1e-3)
+    fc_2 = tf.nn.relu(tf.nn.bias_add(pool2_BN,b_fc2))
 
     # Output Layer
     W_out = weights_initialize([192,10],1/192.0,0.0,"W_out")
